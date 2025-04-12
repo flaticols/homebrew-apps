90a9fe52b6d8b617356f2cec57eb62471f862ca5
Denis Panfilov <gh@flaticols.dev>
2025-04-12 21:32:51 +0200

    Brew formula update for bump version v0.0.7

--- Formula/bump.rb
+++ Formula/bump.rb
@@ -5,21 +5,21 @@
 class Bump < Formula
   desc "Bump semver git tag in yours repository"
   homepage "https://bump.flaticols.dev"
-  version "0.0.6"
+  version "0.0.7"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/flaticols/bump/releases/download/v0.0.6/bump_Darwin_x86_64.tar.gz"
-      sha256 "7a12656a853c1e3b10c48fe927afeec760ff95b7c53913b9b31e7554c7cc5e7d"
+      url "https://github.com/flaticols/bump/releases/download/v0.0.7/bump_Darwin_x86_64.tar.gz"
+      sha256 "3db7c6fc77722ac4019a277292e9af385fa232094fbe8b70b401133946a314ba"
 
       def install
         bin.install "bump"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/flaticols/bump/releases/download/v0.0.6/bump_Darwin_arm64.tar.gz"
-      sha256 "b8f6379f265e8a4a8f0511d4ea744c83169e05bf78d7b0acbdce8167c049fa3a"
+      url "https://github.com/flaticols/bump/releases/download/v0.0.7/bump_Darwin_arm64.tar.gz"
+      sha256 "770e1cf798d5827a73ec20cd7a13fc626b0382a7220930d692479c7721979aa3"
 
       def install
         bin.install "bump"
@@ -30,8 +30,8 @@ class Bump < Formula
   on_linux do
     if Hardware::CPU.intel?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/flaticols/bump/releases/download/v0.0.6/bump_Linux_x86_64.tar.gz"
-        sha256 "cb5887f99b1295f44f0ddb6a92fff52a81aedec5bc4f27b040b1141a1162f8af"
+        url "https://github.com/flaticols/bump/releases/download/v0.0.7/bump_Linux_x86_64.tar.gz"
+        sha256 "a0810eb0d173ac97956ed453b31e622ae7173f0a33ab44c009a7d68b9e0ba0e3"
 
         def install
           bin.install "bump"
@@ -40,8 +40,8 @@ class Bump < Formula
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/flaticols/bump/releases/download/v0.0.6/bump_Linux_arm64.tar.gz"
-        sha256 "bdd860b1c257f988186c468057a5f0c841de90db3853fe8a363afb786295928e"
+        url "https://github.com/flaticols/bump/releases/download/v0.0.7/bump_Linux_arm64.tar.gz"
+        sha256 "7ceaf69df3ee5ad21e80e5649ba84ccafecc760136be8d36e47c23ccb1b51396"
 
         def install
           bin.install "bump"
